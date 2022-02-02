0c05ca0125792bd6f22e75c47c76ce7b634cd1e5
Karl-witha-K <kurba005@fiu.edu>
2022-02-02 16:14:16 -0500

    Formated SHPE Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="election-Dashboard">
-
+    <div class="text-3xl font-bold m-6">Welcome to Panther Votes!</div>
+      <div class="text-xl font-bold m-6">Please Login</div>
+            <div class="text-base font-bold mb-6">Are you here to Vote or Setup an Election?</div>
   </div>
-  
 </template>
+
+
+
