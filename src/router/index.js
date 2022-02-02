ee9f96e48740f1270a5e7030974dd9271cdfc3af
Karl-witha-K <kurba005@fiu.edu>
2022-02-02 14:23:07 -0500

    Added Home Page

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,14 @@ const routes = [
     component: Home
   },
   {
+    path: '/election-Dashboard',
+    name: 'Election-Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Election-Dashboard.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
