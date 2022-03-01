84ef6379021fb49178ec6160aa5e89b0b9fc0f5d
unknown <tylersalzberg@gmail.com>
2022-03-01 17:49:55 -0500

    user login stuff

---
+++ src/services/UserService.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const url = 'http://localhost:5000/api/users'
+
+class UserService{
+
+static Login(Username, Password){
+    return axios.post(url, )}
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,7 +7,7 @@
         <div class="card-body">
           <!-- Makes POST request to /login route -->
           <form action="/login" method="POST">
-            <div class="py-6 form-group">
+            <div class="py-6 form-group"> 
               <label
                 for="email"
                 class="flex flex-wrap justify-center font-bold px-6 mb-2"
@@ -78,6 +78,7 @@
               mx-2
             "
           >
+          
             <svg
               viewBox="0 0 24 24"
               class="fill-current mr-3 w-6 h-5"
@@ -87,8 +88,16 @@
                 d="M12.24 10.285V14.4h6.806c-.275 1.765-2.056 5.174-6.806 5.174-4.095 0-7.439-3.389-7.439-7.574s3.345-7.574 7.439-7.574c2.33 0 3.891.989 4.785 1.849l3.254-3.138C18.189 1.186 15.479 0 12.24 0c-6.635 0-12 5.365-12 12s5.365 12 12 12c6.926 0 11.52-4.869 11.52-11.726 0-.788-.085-1.39-.189-1.989H12.24z"
               />
             </svg>
-            <span class="border-l border-blue-500 h-6 w-1 block"></span>
-            <span class="pl-3">Sign up with Google</span>
+             <div class="col-sm-4">
+      <div class="card">
+        <div class="card-body">
+          <a class="btn btn-block btn-social btn-google" href="/auth/google" role="button">
+            <i class="fab fa-google"></i>
+            Sign In with Google
+          </a>
+        </div>
+      </div>
+    </div>
           </div>
 
           <div
@@ -175,6 +184,7 @@
         </div>
     </div> */
 //min-h-screen
+
 </script>
 
  
