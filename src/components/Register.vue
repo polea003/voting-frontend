42f04d1d631d3a1d86e4a4c2c0956136653e272f
unknown <tylersalzberg@gmail.com>
2022-03-13 23:14:53 -0400

    hep

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,35 +1,43 @@
 <template>
-  <div class="col-md-12">
+   <div class="col-md-12 flex flex-col justify-center items-center mt-5 text-2xl font-bold">
+      <h1 class="text-5xl underline mb-3">Register</h1>
     <div class="card card-container">
-      <img
-        id="profile-img"
-        src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-        class="profile-img-card"
-      />
+    
       <Form @submit="handleRegister" :validation-schema="schema">
         <div v-if="!successful">
           <div class="form-group">
-            <label for="username">Username</label>
-            <Field name="username" type="text" class="form-control" />
+            <label for="username" class="flex flex-wrap justify-center font-bold px-6 mb-2">Name</label>
+            <Field name="username" type="text" placeholder="JohnDoe@fiu.edu" class="form-control w-96 h-10 border-2 border-blue-800 rounded px-2" />
             <ErrorMessage name="username" class="error-feedback" />
           </div>
           <div class="form-group">
-            <label for="email">Email</label>
-            <Field name="email" type="email" class="form-control" />
+            <label for="email" class="flex flex-wrap justify-center font-bold px-6 mb-2">Email</label>
+            <Field name="email" type="email"  placeholder="JohnDoe@fiu.edu" class="form-control w-96 h-10 border-2 border-blue-800 rounded px-2" />
             <ErrorMessage name="email" class="error-feedback" />
           </div>
           <div class="form-group">
-            <label for="password">Password</label>
-            <Field name="password" type="password" class="form-control" />
+            <label for="password" class="flex flex-wrap justify-center font-bold px-6 mb-2">Password</label>
+            <Field name="password" type="password" placeholder="JohnDoe@fiu.edu" class="form-control w-96 h-10 border-2 border-blue-800 rounded px-2" />
             <ErrorMessage name="password" class="error-feedback" />
           </div>
           <div class="form-group">
-            <button class="btn btn-primary btn-block" :disabled="loading">
-              <span
-                v-show="loading"
-                class="spinner-border spinner-border-sm"
-              ></span>
-              Sign Up
+           <button 
+              type="submit"
+              class="
+              mt-9
+                my-5
+                bg-blue-500
+                hover:bg-blue-800
+                text-white
+                hover:text-yellow-300
+                font-bold
+                py-2
+                px-4
+                border border-blue-700
+                rounded
+              "
+            >
+              Sign up
             </button>
           </div>
         </div>
@@ -72,6 +80,7 @@ export default {
         .required("Password is required!")
         .min(6, "Must be at least 6 characters!")
         .max(40, "Must be maximum 40 characters!"),
+        
     });
     return {
       successful: false,
